1c407b70d09ce7e476b653f440e9b180713e4696
Manoel Silva Junior <msilvajrdev@gmail.com>
2026-02-20 23:04:21 -0300

    feat(admin): Configure the display of models in the Django admin panel

--- backend/portfolio/admin.py
+++ backend/portfolio/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile, Skill, Project, Experience, Contact
 
-# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Skill)
+admin.site.register(Project)
+admin.site.register(Experience)
+admin.site.register(Contact)
