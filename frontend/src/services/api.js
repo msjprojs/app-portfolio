8f7a7d02bdefc8a8dce1602174a36d6e39621699
Manoel <msjdevprojeto@gmail.com>
2026-02-27 22:36:48 -0300

    chore(api): config axios instance with baseURL and headers

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -6,5 +6,27 @@ const api = axios.create({
     baseURL: API_BASE_URL,
     headers: {
         'Content-Type': 'application/json'
-    }
-})
+    },
+});
+
+export const portfolioAPI = {
+    // profile
+    getProfile: () => api.get('/profile/main/'),
+
+    // Skills
+    getSkills: () => api.get('/skills/'),
+    getSkillsByCategory: () => api.get('/skills/by_category/'),
+
+    // Projects
+    getProjects: () => api.get('/projects/'),
+    getFeaturedProjects: () => api.get('/projects/featured/'),
+
+    // Experience
+    getExperience: () => api.get('/experience/'),
+
+    // Contact
+    sendMessage: (data) => api.post('contact', data),
+
+}
+
+export default api;
