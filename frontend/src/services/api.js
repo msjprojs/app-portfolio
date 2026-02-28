12e3d3ca2bfdfe98f40cecedad0bb31945d636ac
Manoel <msjdevprojeto@gmail.com>
2026-02-27 22:22:31 -0300

    chore: install axios

---
+++ frontend/src/services/api.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'http://127.0.0.1:8080/api'
+
+const api = axios.create({
+    baseURL: API_BASE_URL,
+    headers: {
+        'Content-Type': 'application/json'
+    }
+})
